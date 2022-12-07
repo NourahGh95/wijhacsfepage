93f08a5bbd364779881e2ef7a9a2040cdc94f32b
Nourah <nourah.abghanim@hotmail.com>
2022-12-07 14:35:52 +0300

    Initial Commit

---
+++ wijha/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  wijha
+//
+//  Created by Nourah on 13/05/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wijha/wijhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  wijhaApp.swift
+//  wijha
+//
+//  Created by Nourah on 13/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct wijhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
